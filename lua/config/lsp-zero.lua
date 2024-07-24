f01407ce6292653b27790d469c7e0fa172fb0473
aszweryn <101809061+aszweryn@users.noreply.github.com>
2024-07-24 10:44:33 +0200

    feat: config for autocomplete

--- lua/config/lsp-zero.lua
+++ lua/config/lsp-zero.lua
@@ -1,4 +1,6 @@
 local lsp_zero = require('lsp-zero')
+local cmp = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
 
 lsp_zero.on_attach(function(client, bufnr)
 	lsp_zero.default_keymaps({buffer = bufnr})
@@ -7,4 +9,28 @@ end)
 lsp_zero.setup_servers({
 	'pyright',
 	'yamlls',
+	'lua-language-server',
+})
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-/>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
 })
