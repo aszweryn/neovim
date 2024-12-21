a0a13499a352702221704a0b9c6d531669b7efb0
aszweryn <101809061+aszweryn@users.noreply.github.com>
2024-12-21 22:47:45 +0100

    feat: overhaul inspired by devops toolbox

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,5 @@
+require('gen').prompts['DevOps me!'] = {
+  prompt = "You are a senior Devops Engineer, acting as an assistant. You offer help with cloud technologies like: Terraform, AWS, Kubernetes, Python. You answer with code examples when possible. $input:\n$text",
+  replace = true
+}
+
