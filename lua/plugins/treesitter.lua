e3999a6f990d20f9cf8386033a9bd2e50ac01d7d
aszweryn <101809061+aszweryn@users.noreply.github.com>
2024-12-22 22:54:19 +0100

    feat(nvim): enable showing hidden files in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,14 +2,19 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 
-    'go', 'lua', 'python', 'rust', 'typescript', 'regex', 
+  ensure_installed = {
+    'go', 'lua', 'python', 'rust', 'typescript', 'regex',
     'bash', 'markdown', 'markdown_inline', 'kdl', 'sql', 'org', 'terraform',
     'html', 'css', 'javascript', 'yaml', 'json', 'toml',
   },
 
   highlight = { enable = true },
   indent = { enable = true },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+  },
   incremental_selection = {
     enable = true,
     keymaps = {
