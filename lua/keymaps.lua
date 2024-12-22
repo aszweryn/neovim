e614213269c84cb4a9ebfddb6a29cb8fe17d544b
aszweryn <101809061+aszweryn@users.noreply.github.com>
2024-12-22 15:13:40 +0100

    feat(keymaps): added some remaps from previous config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,12 +1,13 @@
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap=false})
 -- twilight
 vim.api.nvim_set_keymap("n", "tw", ":Twilight<enter>", {noremap=false})
+
 -- buffers
 vim.api.nvim_set_keymap("n", "tk", ":blast<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "tj", ":bfirst<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "th", ":bprev<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "tl", ":bnext<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", {noremap=false})
+
 -- files
 vim.api.nvim_set_keymap("n", "QQ", ":q!<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "WW", ":w!<enter>", {noremap=false})
@@ -14,31 +15,48 @@ vim.api.nvim_set_keymap("n", "E", "$", {noremap=false})
 vim.api.nvim_set_keymap("n", "B", "^", {noremap=false})
 vim.api.nvim_set_keymap("n", "TT", ":TransparentToggle<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "ss", ":noh<CR>", {noremap=true})
+
 -- listchars toggle
 vim.keymap.set("n", "<leader>lc", "<cmd>set list!<CR>")
---
+
 -- splits
---
 vim.api.nvim_set_keymap("n", "<C-W>,", ":vertical resize -10<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<C-W>.", ":vertical resize +10<CR>", {noremap=true})
 vim.keymap.set('n', '<space><space>', "<cmd>set nohlsearch<CR>")
--- Quicker close split
+
+-- Search terms in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- quicker close split
 vim.keymap.set("n", "<leader>qq", ":q<CR>",
   {silent = true, noremap = true}
 )
 
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
+-- split keybindings
+vim.keymap.set("n", "<leader>sV", "<cmd>split<CR>")
+vim.keymap.set("n", "<leader>sv", "<cmd>vertical split<CR>")
+
+-- quick escape w/ jj in insert mode
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap=false})
+
+-- centering w/ C-D and C-U
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keymaps for better default experience
+-- see `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
--- Remap for dealing with word wrap
+-- remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- Noice
+-- noice
 vim.api.nvim_set_keymap("n", "<leader>nn", ":Noice dismiss<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>cn", ":lua require('notify').dismiss()<CR>", { noremap = true, silent = true })
 
+-- golang remap
 vim.keymap.set("n", "<leader>ee", "<cmd>GoIfErr<cr>",
   {silent = true, noremap = true}
 )
